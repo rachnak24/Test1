66a1cb00193f14189a21488790d36456c71f2e73
rachna <rachna.khanna@live.com>
2020-02-04 20:55:45 -0600

    hello sam

---
+++ src/myMain.java
@@ -0,0 +1,11 @@
+
+public class myMain 
+{
+	
+	public static void myMtehod(int i, String s)
+	{
+		System.out.println("Hello");
+	
+	}
+
+}
---
+++ src/realMain.java
@@ -0,0 +1,18 @@
+
+public class realMain {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int uu = 90;
+		String ss= "Sam";
+		
+	//myMain.myMtehod(10, "Sat");
+	myMain.myMtehod(uu, ss);
+	
+	
+		
+	}
+
+}
